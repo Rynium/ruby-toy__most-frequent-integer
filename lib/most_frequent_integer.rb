fe7775b10d6be3c04724fb8ccdb486dc0a396721
rynium <ryanstantz@gmail.com>
2016-02-25 02:49:32 +0000

    Done

--- lib/most_frequent_integer.rb
+++ lib/most_frequent_integer.rb
@@ -3,5 +3,27 @@
 # exists, return nil.
 
 def find_most_frequent_integer(arr)
-  # put your code here
+    empty = {}
+    max = {}
+    num = nil
+    
+    i = 0 
+    while i < arr.length
+        if empty.has_key?(arr[i]) == true
+            empty[arr[i]] += 1
+        else
+            empty[arr[i]] = 1
+        end
+        i+=1
+    end
+    
+    empty.each do |key,value|
+        if value == empty.values.max
+            max[key] = value
+            num = key
+        end
+    end
+    
+    max.length > 1 ? nil : num
+    
 end
